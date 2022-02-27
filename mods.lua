897834b95638dc50e5f7a219723f8650e807163d
influence0 <88698794+influence0@users.noreply.github.com>
2022-02-27 15:47:08 -0500

    Update mods.lua

--- mods.lua
+++ mods.lua
@@ -4,7 +4,7 @@ MODS = {
 }
 
 ADMIN = {
-    [1860025727] = true; -- 
+    [8] = true; -- 
     [324778090] = true; -- 
 }
 
@@ -12,14 +12,14 @@ function initiateNames()
 	game.Players.LocalPlayer.Character.UpperTorso:FindFirstChild('OriginalSize'):Destroy()
 	for _,v in pairs(game:GetService('Players'):GetPlayers()) do
 		if v.Character then
-			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
-				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
-			end
 			if MODS[v.UserId] then
 				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[⭐] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
 			end
 			if ADMIN[v.UserId] then
 				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
+			end
+			if not v.Character.UpperTorso:FindFirstChild('OriginalSize') then
+				v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[😎] ' .. v.Character:FindFirstChildWhichIsA('Humanoid').DisplayName)
 			end
 		end
 	end
